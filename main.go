358e24a1f5d01d5065424e05f8a4e9658f8afe89
许子恒 <tonatiuh@163.com>
2022-10-29 18:38:21 +0800

    123

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package chatshock
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"chatshock/configs"
+
+)
+
+func main() {
+	r := gin.Default()
+	InitConfig()
+	InitDatabase()
+	InitRedis()
+	InitRouters(r)
+	runHost := configs.Conf.AppConfig.AppHost + ":" + configs.Conf.AppConfig.AppPort
+	err := r.Run(runHost)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
